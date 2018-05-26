b0a84a178a15c3eb1a98e5251677c44d503eaff4
Christofer Ärleryd <c.arleryd@gmail.com>
2018-05-26 18:35:22 +0200

    Update config files

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(flycheck-javascript-flow-args nil)
+ '(helm-mode nil)
+ '(package-selected-packages
+   (quote
+    (company-flow purescript-mode psc-ide evil-numbers evil-escape neotree -mode evil-magit magit php-mode alchemist linum-relative rjsx-mode solidity-mode vue-mode company-mode helm-company tide exec-path-from-shell web-mode javascript-eslint javascript-flow flow-minor-mode js3-mode js2-mode prettier-js flycheck-flow helm-projectile-find-file-in-known-projects helm-projectile-ag helm-ag evil-leader helm-projectile projectile use-package helm evil-visual-mark-mode))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
